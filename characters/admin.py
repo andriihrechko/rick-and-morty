c46c9dde02e0c5a61abf3ecaf13190ecf3fc247d
andriihrechko <andriihrechko250703@gmail.com>
2026-06-14 20:58:56 +0300

    create character model and register in admin page

--- characters/admin.py
+++ characters/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from characters.models import Character
+
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ("api_id", "name", "status", "species", "gender")
+    search_fields = ("name",)
+    list_filter = ("status", "species", "gender")
+    ordering = ("name",)
--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    """Character model from Rick and Morty API."""
+
+    class StatusChoices(models.TextChoices):
+        ALIVE = "Alive"
+        DEAD = "Dead"
+        UNKNOWN = "Unknown"
+
+    class GenderChoices(models.TextChoices):
+        MALE = "Male"
+        FEMALE = "Female"
+        GENDERLESS = "Genderless"
+        UNKNOWN = "Unknown"
+
+    api_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=255, unique=True)
+    status = models.CharField(max_length=7, choices=StatusChoices.choices)
+    species = models.CharField(max_length=255)
+    gender = models.CharField(max_length=10, choices=GenderChoices.choices)
+    image = models.URLField()
+
+    def __str__(self):
+        return self.name
